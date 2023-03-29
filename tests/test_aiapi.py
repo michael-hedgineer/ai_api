1343d3218d88e1390f0d95e1dd1d2721103c360a
Michael Watson <michaelwtsn4@gmail.com>
2023-03-29 17:25:07 -0400

    Add tests that leverage the doc strings

--- tests/test_aiapi.py
+++ tests/test_aiapi.py
@@ -13,9 +13,32 @@ class TestAiApi:
     def ai_api(self):
         api = AiApi()
 
-        @api.register_api('risk_decomposition', api_dict=example1)
-        def test_function(portfolio: list, date: datetime.date):
-            return 'test'
+        @api.register_api(api_dict=example1)
+        def risk_decomposition(portfolio: list, date: datetime.date):
+            '''
+            Runs risk decomposition for an equities portfolio in order to tell a user what their volitility exposures are to different factors.
+            
+            Args:
+                portfolio (list): A list of tuples containing the ticker and the quantity
+                date (datetime.date): The date to run the risk decomposition for
+            
+            Returns:
+                A dict mapping each factor to the dollar volatility in the portfolio.
+                Units are in dollars. The higher the value the higher the risk exposure.
+                
+                example:
+                    {
+                        "Beta_Dollar_Vol": 950000,
+                        "Tech_Dollar_Vol": 400000,
+                        "Momentum_Dollar_Vol": 350000
+                    }
+            '''
+            result = {
+                "Beta_Dollar_Vol": 950000,
+                "Tech_Dollar_Vol": 400000,
+                "Momentum_Dollar_Vol": 350000
+            }
+            return  result
 
         return api
 
@@ -74,36 +97,18 @@ class TestAiApi:
 
         assert len(ai_api._apis) == 1
     
-    def test_ai_prompt_keys_len(self, ai_api, monkeypatch):
-        
-        ai_api._set_apis_prompt()
-        assert len(ai_api._api_prompts) == 3
-    
     def test_answer_prompt_keys(self, ai_api, monkeypatch):
 
         ai_api._set_apis_prompt()
         api_response = ai_api._generate_answer_prompts(['risk_decomposition'])
         for prompt in api_response:
             assert prompt['role'] in ('user', 'assistant', 'system'), f"Invalid key {prompt['role']} in ai prompt keys"
-        
-    def test_answer_prompt_len(self, ai_api, monkeypatch):
-
-        ai_api._set_apis_prompt()
-        api_response = ai_api._generate_answer_prompts(['risk_decomposition'])
-        assert len(api_response) == 3
 
     def test_answer_prompt_keys_len(self, ai_api, monkeypatch):
 
         ai_api._set_apis_prompt()
         api_response = ai_api._generate_answer_prompts(['risk_decomposition'])
         assert len(api_response[0]) == 2
-
-    def test_register_api_mismatched_names(self, ai_api, monkeypatch):
-
-        with pytest.raises(AssertionError):
-            @ai_api.register_api('mismatched_name', api_dict=example1)
-            def mismatched_function():
-                return 'test'
             
     def test_generate_answer_prompts_nonexistent_api(self, ai_api, monkeypatch):
 
